43409bb409ab629b9f3e9b8aca3cde5258aad98a
Darryl Allardice <duderdice@gmail.com>
2017-06-14 19:29:11 -0400

    minor cleanups

--- az.ps1
+++ az.ps1
@@ -2,10 +2,27 @@
 # (may require 'az login' steps in order to log into azure and set azure context)
 
 
+# create deployment user
+# NOTE - only need to do this once, can use for all Azure deployments
+UserName=secret!
+Password=secret!
+#az appservice web deployment user set --user-name ${UserName} --password ${Password}
+az webapp deployment user set --user-name ${UserName} --password ${Password}
+
 # create resource groups for app
-MyResourceGroup=MatrixServicesAPI
+MyResourceGroup=Matrix-SampleAngularApp
 MyLocation=eastus
 az group create --name ${MyResourceGroup} --location ${MyLocation}
+
+# create service plan
+MyAppServicePlan=serviceplanX
+az appservice plan create --name ${MyAppServicePlan} --resource-group ${MyResourceGroup} --sku FREE
+
+# create web app within the service plan
+MyAppName=Matrix-SampleAngularApp
+#az appservice web create --name ${MyAppName} --resource-group ${MyResourceGroup} --plan ${MyAppServicePlan}
+az webapp create --name ${MyAppName} --resource-group ${MyResourceGroup} --plan ${MyAppServicePlan}
+
 
 # various ways to query; use "--output table" to get data in tabular form instead of json
 az group list
@@ -17,6 +34,10 @@ az group list --query "[?name=='${MyResourceGroup}']"
 
 az resource list
 az resource list --output table
+
+
+az webapp list --output table
+az webapp list --output table
 
 
 # delete resource groups for app
