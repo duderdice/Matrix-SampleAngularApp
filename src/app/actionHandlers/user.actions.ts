baaea7c92b844a0e9526309de5708314dd2f6926
Sorum Panchal <Sorum.Panchal@matrixres.com>
2017-07-12 09:22:08 -0400

    Deleting commented-out code

--- src/app/actionHandlers/user.actions.ts
+++ src/app/actionHandlers/user.actions.ts
@@ -45,31 +45,4 @@ export class UserActions {
             });
     }
 
-    // ------------------------------------------------------------------------------------------------
-    // mock services
-
-//     private submitPaymentTrxMock(paymentTrxRequest: PaymentTrxRequest): PaymentTrxResponse {
-//         const goodPaymentTrxResponse: PaymentTrxResponse = {
-//             id: '12345',
-//             trxDateTime: moment().format('LLLL'),
-//             isSuccessful: true,
-//             trxAmount: paymentTrxRequest.paymentAmount,
-//             confirmationCode: 'A12345',
-//             errorCode: '',
-//             errorMessage: '',
-//             paymentTrxRequest,
-//         };
-//         const badPaymentTrxResponse: PaymentTrxResponse = {
-//             id: '12345',
-//             trxDateTime: moment().format('LLLL'),
-//             isSuccessful: false,
-//             trxAmount: paymentTrxRequest.paymentAmount,
-//             confirmationCode: '',
-//             errorCode: 'Z509',
-//             errorMessage: 'Insufficient Funds',
-//             paymentTrxRequest,
-//         };
-//         const random = Math.random();
-//         return (random > 0.5) ? goodPaymentTrxResponse : badPaymentTrxResponse;
-//     }
 }
