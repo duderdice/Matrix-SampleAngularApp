e1a0517182586307851b13a6cfe428d9a06de44f
Hassan Masroor <Hassan.Masroor@me.com>
2017-08-07 14:22:40 -0400

    api service refactored using new httpClient implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { MomentModule } from 'angular2-moment';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
@@ -32,6 +33,7 @@ import { MockHttpModule } from './mocks/mock-http.module';
     StoreModule.provideStore(APP_STORES),
     StoreDevtoolsModule.instrumentOnlyWithExtension({ maxAge: 5 }),
     ToasterModule,
+    HttpClientModule
   ],
   providers: [
     ...APP_ACTION_HANDLERS,
